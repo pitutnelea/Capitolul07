f5f0002384845ad220b588f2ad50823d33aa7144
Pitut Nelea <pitut.nelea@yahoo.com>
2019-12-04 23:06:53 +0200

    -

--- Tema7.2-Doua_clase/main.cpp
+++ Tema7.2-Doua_clase/main.cpp
@@ -49,3 +49,58 @@ int main()
 
     return 0;
 }
+
+/*Creați o clasă, Candelabru.
+
+Declarați ca private următorii membri de tip date:
+    unsigned short nrBecuri
+    BecReglabil* listaBecuri
+
+Mai declarați și următorii membri publici de tip metoda:
+a)ConfigureazaCandelabru
+    primește numărul de becuri al candelabrului sub forma unui parametru unsigned short
+    inițializează corespunzător membrul nrBecuri
+    inițializează listaBecuri prin crearea pe heap a unui array cu numărul necesar de elemente
+    inițializează fiecare bec din listă punându-i puteream curentă pe 0 și cea maximă pe 100
+    nu returnează nimic
+b)AprindeLumina
+    nu primește parametri
+    pune la fiecare bec puterea curentă pe maxim și membrul aprins pe true
+    nu returnează nimic
+    StingeLumina
+    nu primește parametri
+    pune la fiecare bec puterea curentă pe 0 și membrul aprins pe false
+    nu returnează nimic
+c)MaresteLumina
+    primește un parametru de tip unsigned short
+    crește valoarea lui putereCurenta a fiecărui bec cu parametrul primit
+    nu returnează nimic
+    ReduceLumina
+    primește un parametru de tip unsigned short
+    scade valoarea lui putereCurenta a fiecărui bec cu parametrul primit
+    nu returnează nimic
+d)PutereMaximaCandelabru
+    nu primește parametri
+    returneză suma puterilor maxime ale becurilor din listă
+    StareCandelabru
+    nu primește parametri
+    returneză true dacă cel puțin un bec are membrul aprins pe true. Dacă nu, returnează false
+
+Pentru BecReglabil refolosiți clasa din tema anterioară.
+În acest scop copiați fișierele clasei BecReglabil (.h și .cpp) din celălalt proiect în folderele include respectiv src ale proiectului curent.
+Apoi pe fiecare din cele două foldere (src și include) faceți click dreapta, alegeți din meniul contextual „Add file” și selectați fișierul dorit.
+
+În funcția main declarați două obiecte de tip Candelabru, cand3 și cand5.
+Apelați metoda ConfigureazaCandelabru pentru ambele obiecte cu parametru 3, respectiv 5 (numărul de becuri al acelui obiect Candelabru)
+Afișați starea fiecărui obiect Candelabru ( dacă e aprins sau stins)
+Apelați metoda  AprindeLumina pentru  ambele obiecte
+Afișați starea fiecărui obiect Candelabru ( dacă e aprins sau stins)
+Apelați metoda  StingeLumina pentru  ambele obiecte
+Afișați starea fiecărui obiect Candelabru ( dacă e aprins sau stins)
+Apelați metoda  MaresteLumina pentru  ambele obiecte cu parametru 150
+Afișați starea fiecărui obiect Candelabru ( dacă e aprins sau stins)
+Apelați metoda  ReduceLuminapentru  ambele obiecte cu parametru 75
+Afișați starea fiecărui obiect Candelabru ( dacă e aprins sau stins)
+Apelați metoda  ReduceLuminapentru  ambele obiecte cu parametru 30
+Afișați starea fiecărui obiect Candelabru ( dacă e aprins sau stins)
+Afișați puterea maximă a  fiecărui obiect Candelabru*/
